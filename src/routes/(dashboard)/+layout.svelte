01708194ab98dc1c018ca028f21bbcae88c86152
unknown <its.saran.pv@gmail.com>
2023-11-23 18:29:51 +0530

    First commit

---
+++ src/routes/(dashboard)/+layout.svelte
@@ -0,0 +1,26 @@
+<div class="grid grid-cols-12 gap-x-16 bg-slate-200 min-h-screen">
+	<header class="col-span-3 text-center bg-violet-900">
+		<div>
+			<a href="/invoices">
+				<img src="/images/logo.svg" alt="The Dollar Board" class="mx-auto" />
+			</a>
+		</div>
+		<nav>
+			<ul class="list-none text-2xl font-bold">
+				<li class="mb-6">
+					<a href="/invoices" class="font-bold text-white hover:text-yellow-500">Invoices</a>
+				</li>
+				<li><a href="/clients">Clients</a></li>
+				<li><a href="#">Settings</a></li>
+				<li><a href="#">Logout</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main><slot /></main>
+</div>
+
+<!-- <style lang="postcss">
+	nav ul li {
+		@apply;
+	}
+</style> -->
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    fontFamily: {
+      'sansSerif': ['Source Sans 3', 'sans-serif'],
+      'mono': ['Source Code Pro', 'monospace'],
+      'handWriting' : ['Kalam', 'cursive']
+    },
+    extend: {
+      colors: {}
+    },
+  },
+  plugins: [],
+}
+
